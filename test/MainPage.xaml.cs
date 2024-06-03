feb3688e418750feffc68e564e6a2dd118f8c6c1
Twice <pingzvikke@gmail.com>
2024-06-03 13:45:48 +0200

    update

--- test/MainPage.xaml.cs
+++ test/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 
 {
     using System.ComponentModel;
-    
+
 
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
@@ -73,19 +73,37 @@
             BindingContext = this;
         }
 
-        protected void AddCounter(object sender, EventArgs args)
+
+
+        protected void AddPushUp(object sender, EventArgs args)
         {
 
             //Pushup Max Increase
-            if(this.PushUps < 100) 
-            { 
+            if (this.PushUps < 100)
+            {
                 this.PushUps += 1;
             }
-            else if(this.PushUps == 100)
+            else if (this.PushUps == 100)
             {
                 this.PushUps = 100;
             }
+        }
 
+        protected void SubPushUp(object sender, EventArgs args)
+        {
+            //Pushups Decrease
+            if (this.PushUps >= 1)
+            {
+                this.PushUps -= 1;
+            }
+            else if (this.PushUps == 0) //Its not going down to negative
+            {
+                this.PushUps = 0;
+            }
+        }
+
+        protected void AddSitUp(object sender, EventArgs args)
+        {
             //Situps Max Increase
             if (this.SitUps < 100)
             {
@@ -95,41 +113,10 @@
             {
                 this.SitUps = 100;
             }
-
-            //Squats Max Increase
-            if (this.Squats < 100)
-            {
-                this.Squats += 1;
-            }
-            else if (this.Squats == 100)
-            {
-                this.PushUps = 100;
-            }
-
-            //KM Max Increase
-            if (this.Kilometers < 10)
-            {
-                this.Kilometers += 1;
-            }
-            else if (this.Kilometers == 10)
-            {
-                this.Kilometers = 10;
-            }
         }
 
-        protected void SubCounter(object sender, EventArgs args)
+        protected void SubSitUp(object sender, EventArgs args)
         {
-
-            //Pushups Decrease
-            if (this.PushUps >= 1) 
-            {
-                this.PushUps -=1;
-            }
-            else if(this.PushUps == 0) //Its not going down to negative
-            {
-                this.PushUps = 0;
-            }
-
             //Situps Decrease
             if (this.SitUps >= 1)
             {
@@ -140,7 +127,23 @@
                 this.SitUps = 0;
             }
 
-            //Squats Decrease
+        }
+
+        protected void AddSquat(object sender, EventArgs args)
+        {
+            //Squats Max Increase
+            if (this.Squats < 100)
+            {
+                this.Squats += 1;
+            }
+            else if (this.Squats == 100)
+            {
+                this.PushUps = 100;
+            }
+        }
+
+        protected void SubSquat(object sender, EventArgs args)
+        {
             if (this.Squats >= 1)
             {
                 this.Squats -= 1;
@@ -150,59 +153,30 @@
                 this.Squats = 0;
             }
 
-            //KM Decrease
-            if (this.Kilometers >= 1)
-            {
-                this.Kilometers -= 1;
-            }
-            else if (this.Kilometers == 0) //Its not going down to negative
-            {
-                this.Kilometers = 0;
-            }
-
-
-
-        }
-
-        protected void AddPushUp(object sender, EventArgs args)
-        {
-            this.PushUps += 1;
-        }
-
-        protected void SubPushUp(object sender, EventArgs args)
-        {
-            this.PushUps -= 1;
-        }
-
-        protected void AddSitUp(object sender, EventArgs args)
-        {
-            this.SitUps += 1;
-        }
-
-        protected void SubSitUp(object sender, EventArgs args)
-        {
-            this.SitUps -= 1;
-        }
-
-        protected void AddSquat(object sender, EventArgs args)
-        {
-            this.Squats += 1;
-        }
-
-        protected void SubSquat(object sender, EventArgs args)
-        {
-            this.Squats -= 1;
         }
 
         protected void AddKilometer(object sender, EventArgs args)
         {
-            this.Kilometers += 1.0;
+            if (this.Kilometers < 10)
+            {
+                this.Kilometers += 1;
+            }
+            else if (this.Kilometers == 10)
+            {
+                this.Kilometers = 10;
+            }
         }
 
         protected void SubKilometer(object sender, EventArgs args)
         {
-            this.Kilometers -= 1.0;
+            if (this.Kilometers >= 10)
+            {
+                this.Kilometers -= 1;
+            }
+            else if (this.Kilometers == 0)
+            {
+                this.Kilometers = 0;
+            }
         }
     }
-
 }
